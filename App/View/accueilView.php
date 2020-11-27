29b365073e68abf6f7d366b429176471acf58da8
TheMadSandman <martindeboutray@hotmail.fr>
2020-11-27 16:00:03 +0100

    Implémentation du composant header

--- App/View/accueilView.php
+++ App/View/accueilView.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page accueil</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    
+    <?php include "./include/header.php"; ?>
+    test
 </body>
 </html>
