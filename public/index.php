f8dd76b7d8384b60efd0dd558ff56d7992dad0e0
axel <axel.zidane@edu.devinci.fr>
2020-12-01 11:19:08 +0100

    nv test

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
+
+
+<?php
+//define("ROOT", dirname(__DIR__));
+//require ROOT."/Autoloader.php";
+
+//Démarre une session de sauvegarde de données.
+//session_start();
+//Autoloader::register();
+
+//require ROOT."/router.php";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
